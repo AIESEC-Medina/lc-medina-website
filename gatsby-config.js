acaa5febc14de520ab4579deb731deb20f8d6669
Nabil-MEDIOUNI <nabilmediouni8@gmail.com>
2021-08-11 23:01:19 +0100

    fix: Icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         background_color: '#fff',
         theme_color: '#fff',
         display: 'standalone',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-gatsby-cloud',
