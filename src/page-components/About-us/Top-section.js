7f5119f1efac9734387d820ca4938d9908141813
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-18 06:22:08 +0100

    feat: Complete the Top-section-about-us

--- src/page-components/About-us/Top-section.js
+++ src/page-components/About-us/Top-section.js
@@ -1,7 +1,33 @@
-import React from 'react';
+/* -------------------------------------------------------------------------- */
+/*                                Dependencies                                */
+/* -------------------------------------------------------------------------- */
 
+// Packages
+
+import React from 'react';
+import Logo from '../../assets/icons/LogoMedina.png';
+// Style
+import './index.scss';
+/* -------------------------------------------------------------------------- */
+/*                                  component                                 */
+/* -------------------------------------------------------------------------- */
 function TopSection() {
-  return <div className="TopSection-container" />;
+  return (
+    <div className="TopSection-container">
+      <div className="Informations-container">
+        <img src={Logo} alt="MedinaLogo" />
+        <div className="intro-info">
+          <h1>develop yourself with aiesec.</h1>
+          <h2>We are a youth leadership movement</h2>
+          <p>
+            We are passionately driven by one cause, peace and fulfillment of
+            humankinds potential.
+          </p>
+          <h3>in Tunisia since 1989</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TopSection;
--- src/pages/About-us.js
+++ src/pages/About-us.js
@@ -1,10 +1,16 @@
+/* -------------------------------------------------------------------------- */
+/*                                Dependencies                                */
+/* -------------------------------------------------------------------------- */
 import React from 'react';
+// Page wrapper
 import Layout from '../shared/Layout/index';
 import TopSection from '../page-components/About-us/Top-section';
+import Seo from '../shared/Seo/index';
 
 function AboutUs() {
   return (
     <>
+      <Seo title="About-Us" />
       <Layout>
         <TopSection />
       </Layout>
