c9f960210c1a7b1f7545ed6186601d277c0813b8
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-18 01:07:57 +0100

    feat: Set up files about us top section

---
+++ src/page-components/About-us/Top-section.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TopSection() {
+  return <div className="TopSection-container" />;
+}
+
+export default TopSection;
---
+++ src/pages/About-us.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../shared/Layout/index';
+import TopSection from '../page-components/About-us/Top-section';
+
+function AboutUs() {
+  return (
+    <>
+      <Layout>
+        <TopSection />
+      </Layout>
+    </>
+  );
+}
+
+export default AboutUs;
