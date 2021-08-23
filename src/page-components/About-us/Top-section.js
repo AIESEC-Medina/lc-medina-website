7ea9c3035d97dff48a82fa3639e854ed6d7c25dc
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-23 21:56:00 +0100

    feat: Typing annimation

--- src/page-components/About-us/Top-section.js
+++ src/page-components/About-us/Top-section.js
@@ -5,6 +5,7 @@
 // Packages
 
 import React from 'react';
+import Typical from 'react-typical';
 import Logo from '../../assets/icons/LogoMedina.png';
 // Style
 import './index.scss';
@@ -17,7 +18,22 @@ function TopSection() {
       <div className="Informations-container">
         <img src={Logo} alt="MedinaLogo" />
         <div className="intro-info">
-          <h1>develop yourself with aiesec.</h1>
+          <h1>
+            <Typical
+              loop={Infinity}
+              wrapper="w"
+              steps={[
+                'intership with aiesec',
+                1000,
+                'teach with aiesec',
+                1000,
+                'membership with aiesec',
+                1000,
+                'develop yourself with aiesec',
+                1000,
+              ]}
+            />
+          </h1>
           <h2>We are a youth leadership movement</h2>
           <p>
             We are passionately driven by one cause, peace and fulfillment of
