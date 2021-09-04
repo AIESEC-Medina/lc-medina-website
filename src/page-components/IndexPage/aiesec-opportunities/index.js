b9fbda4e0a972bf996a56cc3203711a7aa0e5f1c
Ibrahim Doghri <patron.bara@gmail.com>
2021-09-05 00:07:39 +0100

    feat: Added facebook page embed

--- src/page-components/IndexPage/aiesec-opportunities/index.js
+++ src/page-components/IndexPage/aiesec-opportunities/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* -------------------------------------------------------------------------- */
 /*                                Dependencies                                */
 /* -------------------------------------------------------------------------- */
@@ -19,7 +20,33 @@ function AIESECOpportunities() {
     <div className="aiesec-opps-section flex flex-column items-center">
       <p className="heading">AIESEC Opportunities</p>
       <div className="opps-container">
-        <p>here goes ibrahim!</p>
+        {/* <p>here goes ibrahim!</p> */}
+        <span className="embed-container">
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAIntTeam&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId"
+            // width="300"
+            height="500"
+            title="AI team"
+            style={{ border: 'none', overflow: 'hidden' }}
+            scrolling="no"
+            frameBorder="0"
+            allowFullScreen="true"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          />
+        </span>
+        <span className="embed-container">
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAIESECinTunisia5&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId"
+            // width="300"
+            height="500"
+            title="Aiesec Medina"
+            style={{ border: 'none', overflow: 'hidden' }}
+            scrolling="no"
+            frameBorder="0"
+            allowFullScreen="true"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          />
+        </span>
       </div>
     </div>
   );
